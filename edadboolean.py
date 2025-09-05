375fc40bdefa063fa3a5a98b2f0cc2a8d7541018
Gigi-mex-ops <bardalesladis5@gmail.com>
2025-09-06 01:29:59 +0400

    edadboolean.py

---
+++ edadboolean.py
@@ -0,0 +1,15 @@
+""" otro ejercicio el cual me pide edad si soy menor 123 y si soy mayor nada ok o como sea
+si no cumple las dos sera DENEGADOO!
+"""
+
+edad = int(input("es mayor de edad"))
+
+Contrasena = input("te pido la contrasena mas te vale darmela")
+
+if edad >= 18 and Contrasena == "1234":
+ print("edad correcta y adulta pasele")
+ 
+else:
+    print("Eres menor LLamame a un adulto ahora!!!")
+
+    
